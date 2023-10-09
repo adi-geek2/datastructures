ff08241a1c2a357fbc6ac6e677e0a5ee9b0e40a4
unknown <adi2271188@gmail.com>
2023-10-09 17:32:24 +0200

    Added part 1 array ADT functions

--- array_ADT.c
+++ array_ADT.c
@@ -3,7 +3,7 @@
 
 struct Array
 {
-    int *A;
+    int A[10];
     int size;
     int length;
 };
@@ -17,28 +17,84 @@ void Display(struct Array arr)
         printf("%d \n", arr.A[i]);
     }
     
-};
+}
 
+void Append(struct Array *arr, int value)
+{
+    if (arr->length < arr->size)
+        arr->A[arr->length] = value;
+        arr->length++;    
+}
 
+void Insert(struct Array *arr, int index, int value)
+{
+    int i;
+    if(index >= 0 && index < arr->length)
+        for (i=arr->length;i > index; i--)
+            arr->A[i] = arr->A[i-1];
+        arr->A[index] = value;
+        arr->length++;
+}
+
+int Delete(struct Array *arr, int index)
+{
+    int i, deleted_value = 0;
+    if(index >= 0 && index < arr->length)
+        deleted_value = arr->A[index];
+        for (i=index;i < arr->length-1; i++)
+            arr->A[i] = arr->A[i+1];
+        arr->length--;
+    return deleted_value;
+}
+
+void swap (int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(struct Array *arr, int value){
+    for(int i = 0; i < arr->length; i++)
+    {
+        if (arr->A[i] == value){
+            swap(&arr->A[i], &arr->A[0]);
+            return i;        
+        }
+    }
+    return -1;    
+}
 
 int main(){
-    struct Array arr;
-    int n,i;
-
+    struct Array arr ={{2,3,4,5,6},10,5};
+    int i;
+    /*	
     printf("Enter size of an array");
     scanf("%d",&arr.size);
     arr.A=(int*)malloc(sizeof(int)*arr.size);
     arr.length=0;
 
     printf("Enter number of input numbers");
-    scanf("%d",&n);
+    scanf("%d",&arr.length);
 
-    for (size_t i = 0; i < n; i++)
+    for (size_t i = 0; i < arr.length; i++)
     {
         scanf("%d",&arr.A[i]);
-    }
+    }*/
 
+    /*
+    Append(&arr, 10);
+    printf("AFter appending: \n");
     Display(arr);
+    Insert(&arr, 2, 20);
+    printf("AFter insertion: \n");
+    Display(arr);
+    int del_value = Delete(&arr, 2);
+    printf("AFter deletion: \n");
+    Display(arr);
+    printf("Deleted value is : %d", del_value);
+    */
+    printf("element found at %d \n", LinearSearch(&arr, 4));
+    Display(arr);   
 
     return 0;
     
