37db4371b28a06cc807ae7da185eda94e804ce76
adi-geek2 <adi2271188@gmail.com>
2023-10-13 12:21:20 +0200

    Add binary search

--- array_ADT.c
+++ array_ADT.c
@@ -64,10 +64,31 @@ int LinearSearch(struct Array *arr, int value){
     return -1;    
 }
 
+int BinarySearch(struct Array arr, int key){
+    int low = 0, mid, high;
+    high = arr.length-1;
+    mid = (low + high)/2;
+
+    while (low < high)
+    {
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid]){
+            high = mid - 1;
+            mid = (low + high)/2;
+        }
+        else{
+            low = mid + 1;
+            mid = (low + high)/2;        
+        }
+    }
+    return -1;
+}
+
 int main(){
     struct Array arr ={{2,3,4,5,6},10,5};
-    int i;
     /*	
+    int i;
     printf("Enter size of an array");
     scanf("%d",&arr.size);
     arr.A=(int*)malloc(sizeof(int)*arr.size);
@@ -79,9 +100,7 @@ int main(){
     for (size_t i = 0; i < arr.length; i++)
     {
         scanf("%d",&arr.A[i]);
-    }*/
-
-    /*
+    }
     Append(&arr, 10);
     printf("AFter appending: \n");
     Display(arr);
@@ -92,10 +111,10 @@ int main(){
     printf("AFter deletion: \n");
     Display(arr);
     printf("Deleted value is : %d", del_value);
-    */
     printf("element found at %d \n", LinearSearch(&arr, 4));
     Display(arr);   
-
+    */
+    printf("element found at %d \n", BinarySearch(arr, 2));
     return 0;
-    
+      
 }
